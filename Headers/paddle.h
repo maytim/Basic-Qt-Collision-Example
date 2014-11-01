92533708879b6c392c9009d5c7d41f2332f3ea24
Tim <timothy.maytom@gmail.com>
2014-11-01 13:37:10 -0700

    Add comments

--- Headers/paddle.h
+++ Headers/paddle.h
@@ -1,3 +1,7 @@
+/*
+    The Paddle class is another GameObject in the game. This is the object that the player will control to deflect the 
+    Ball object. It has the ability to move left or right based on player input.
+*/
 #ifndef PADDLE_H
 #define PADDLE_H
 
@@ -5,10 +9,13 @@
 
 class Paddle : public GameObject{
 public:
+    //Constructor
     Paddle();
-    ~Paddle();
-
+    
+    //Reset to defaults
     void resetState();
+    
+    //Functions to update the position of the Paddle
     void moveLeft(int);
     void moveRight(int);
 };
